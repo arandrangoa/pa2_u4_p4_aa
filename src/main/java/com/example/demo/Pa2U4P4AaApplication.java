3890efca65f57bc890fc05bf0dce19927be35d1b
Alex Andrango <arandrangoa@uce.edu.ec>
2023-08-14 19:52:05 -0500

    Taller38 Creacion de proyecto

---
+++ src/main/java/com/example/demo/Pa2U4P4AaApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U4P4AaApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U4P4AaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
